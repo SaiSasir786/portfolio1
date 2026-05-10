7fad4462fa17664daff7b45213f54e24f376edff
Sai Sasir K <saisasir99@gmail.com>
2026-05-10 17:38:12 +0530

    Add basepath to router

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export function getRouter() {
     scrollRestoration: true,
     defaultPreload: 'intent',
     defaultPreloadStaleTime: 0,
+    basepath: import.meta.env.BASE_URL,
   })
 
   return router
